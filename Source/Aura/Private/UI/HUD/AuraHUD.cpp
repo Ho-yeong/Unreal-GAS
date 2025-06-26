d8b8b62f2c89acfd7fdcd9ffeec172ef62efd9c0
HoyeongJo <ghdud0503@gmail.com>
2025-06-26 20:02:50 +0900

    feature: HUD

---
+++ Source/Aura/Private/UI/HUD/AuraHUD.cpp
@@ -0,0 +1,14 @@
+// Copyright Studio3F
+
+
+#include "UI/HUD/AuraHUD.h"
+
+#include "Blueprint/UserWidget.h"
+
+void AAuraHUD::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UUserWidget* Widget = CreateWidget<UUserWidget>(GetWorld(), OverlayWidgetClass);
+	Widget->AddToViewport();
+}
