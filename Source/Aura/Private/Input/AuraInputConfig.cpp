994c6220d17bc4bd4a4a062ca1816da8a4a62363
HoyeongJo <ai.switchup@gmail.com>
2025-12-21 21:41:00 +0900

    feature: Custom Input Component, Input Config. Activating Input for Abilities

---
+++ Source/Aura/Private/Input/AuraInputConfig.cpp
@@ -0,0 +1,24 @@
+// Copyright Studio3F
+
+
+#include "Input/AuraInputConfig.h"
+#include "Aura/AuraLogChannels.h"
+
+const UInputAction* UAuraInputConfig::FindAbilityInputActionForTag(const FGameplayTag& InputTag,
+	bool bLogNotFound) const
+{
+	for (const FAuraInputAction& Action: AbilityInputActions)
+	{
+		if (Action.InputAction && Action.InputTag == InputTag)
+		{
+			return Action.InputAction;
+		}
+	}
+	
+	if (bLogNotFound)
+	{
+		UE_LOG(LogAura, Error, TEXT("Can't find AbilityInputAction for InputTag[%s], on InputConfig [%s]"), *InputTag.ToString(), *GetNameSafe(this));
+	}
+	
+	return nullptr;
+}
